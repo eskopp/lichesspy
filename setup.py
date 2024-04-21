5d0d7aa3dafb6a8385de89e1a9076a641a979d05
Erik Skopp <skopp.erik@gmail.com>
2024-04-21 20:54:30 +0200

    New Block Version

--- setup.py
+++ setup.py
@@ -1,32 +1,34 @@
 import setuptools
+from pathlib import Path
 
 
-def def_requirements():
-    """Check PIP Requirements"""
-    pip_lines = ""
+def load_requirements():
+    """Load and return list of requirements from a file."""
+    requirements_path = Path("requirements.txt")
     try:
-        with open("requirements.txt", encoding="utf-8") as file_content:
-            pip_lines = file_content.read().splitlines()
+        return requirements_path.read_text(encoding="utf-8").splitlines()
     except Exception as error:
-        print(f"Execpiton: {error}")
-    return pip_lines
+        # Depending on how critical this file is, you may want to raise an error
+        # raise RuntimeError(f"Unable to read the requirements file: {error}")
+        print(f"Exception when reading requirements: {error}")
+        return []
 
 
-def def_readme():
-    """Check Readme RST"""
-    readme = ""
-    with open("README.rst", encoding="utf-8") as file_content:
-        readme = file_content.read()
-    return readme
+def load_readme():
+    """Load and return the content of the README file."""
+    readme_path = Path("README.rst")
+    if readme_path.exists():
+        return readme_path.read_text(encoding="utf-8")
+    return "Long description could not be read from README.rst"
 
 
 setuptools.setup(
     name="lichesspy",
-    version="6.0.4.dev1",
+    version="6.0.4.dev2",
     author="eskopp",
-    author_email="skopp.erik@gmail.com",
+    author_email="skopp.erik+lichesspy@gmail.com",
     description="Python wrapper for lichess",
-    long_description=def_readme(),
+    long_description=load_readme(),
     long_description_content_type="text/markdown",
     license="MIT",
     url="https://github.com/eskopp/lichesspy",
@@ -39,5 +41,5 @@ setuptools.setup(
         "Operating System :: OS Independent",
     ],
     python_requires=">=3.10",
-    install_requires=def_requirements(),
+    install_requires=load_requirements(),
 )
